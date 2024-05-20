9aa1ffdbec48c6c0fc9e2c85779d42a7d959dc31
rafael <august.rafael@gmail.com>
2024-05-20 19:06:12 -0400

    Modelo e admin de pedidos

--- pedido/admin.py
+++ pedido/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from pedido.models import ItemPedido, Pedido
 
-# Register your models here.
+class ItemPedidoInLine(admin.TabularInline):
+    model = ItemPedido
+    extra = 1
+
+class PedidoAdmin(admin.ModelAdmin):
+    line = [
+        ItemPedidoInLine
+    ]
+
+admin.site.register(Pedido, PedidoAdmin)
+admin.site.register(ItemPedido)
