a73e819f68ef8be8b4fce1e5db9c863b0f56611d
rafael <august.rafael@gmail.com>
2024-06-03 11:55:02 -0400

    novos metodos remove_aspas, cart_total_qtd, cart_total_preco

--- produto/templatetags/omfilters.py
+++ produto/templatetags/omfilters.py
@@ -5,3 +5,17 @@ register = Library()
 @register.filter
 def formata_preco(val):
     return f'R$ {val:.2f}'.replace('.', ',')
+
+@register.filter
+def remove_aspas(val):
+    return str(val).replace('"', '')
+
+@register.filter
+def cart_total_qtd(carrinho):
+    return sum([item['quantidade'] for item in carrinho.values()])
+
+@register.filter
+def cart_total_preco(carrinho):
+    #soma_preco = sum([item['preco_quantitativo'] for item in carrinho.values()])
+    soma_preco_promocional = sum([item['preco_quantitativo_promocional'] for item in carrinho.values()]) 
+    return soma_preco_promocional 
