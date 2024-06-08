aa994a8bfa7a4fb9ea7579c4616ac257d9feb4f2
rafael <august.rafael@gmail.com>
2024-06-07 20:49:47 -0400

    views do perfil

--- perfil/views.py
+++ perfil/views.py
@@ -1,9 +1,101 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.views import View
+from . import forms
+from .models import PerfilUsuario
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
-class Criar(View):
-    pass
+import copy
+
+class BasePerfil(View):
+    
+    template_name = 'perfil/criar.html'
+    
+    def setup(self, *args, **kwargs):
+        super().setup(*args, **kwargs)
+        
+        self.carrinho = copy.deepcopy(self.request.session.get('carrinho'), {})
+        self.perfil = None
+        
+        if self.request.user.is_authenticated:
+            self.perfil = PerfilUsuario.objects.filter(usuario=self.request.user).first()
+            self.context = {
+                'userform': forms.UserForm(
+                                data=self.request.POST or None, 
+                                usuario=self.request.user,
+                                instance=self.request.user,    
+                            ),
+                'perfilform': forms.PerfilForm(data=self.request.POST or None)
+            }
+        else:
+            self.context = {
+                'userform': forms.UserForm(data=self.request.POST or None),
+                'perfilform': forms.PerfilForm(data=self.request.POST or None)
+            }
+            
+        self.userform = self.context['userform']
+        self.perfilform = self.context['perfilform']
+                
+        self.renderizar = render(self.request, self.template_name, self.context)
+    
+    def get(self, *args, **kwargs):        
+        return self.renderizar
+
+class Criar(BasePerfil):
+    
+    def post(self, *args, **kwargs):
+        
+        if not self.userform.is_valid() or not self.perfilform.is_valid():
+            return self.renderizar
+        
+        username = self.userform.cleaned_data.get('username')
+        password = self.userform.cleaned_data.get('password')
+        email = self.userform.cleaned_data.get('email')
+        first_name = self.userform.cleaned_data.get('first_name')
+        last_name = self.userform.cleaned_data.get('last_name')
+        
+        if self.request.user.is_authenticated:
+            usuario = get_object_or_404(User, username=self.request.user.username)
+             
+            usuario.username = username
+            if password:
+               usuario.set_password(password)               
+            usuario.email = email 
+            usuario.first_name = first_name
+            usuario.last_name = last_name
+            usuario.save()
+            
+            if not self.perfil:
+                self.perfilform.cleaned_data['usuario'] = usuario
+                perfil = PerfilUsuario(**self.perfilform.cleaned_data)
+                perfil.save()
+            else:
+                perfil = self.perfilform.save(commit=False)
+                perfil.usuario = usuario
+                perfil.save()
+        else:
+            usuario = self.userform.save(commit=False)
+            usuario.set_password(password)
+            usuario.save()
+            
+            perfil = self.perfilform.save(commit=False)
+            perfil.usuario = usuario
+            perfil.save()
+        
+        if password:
+            authentica = authenticate(
+                self.request,
+                username=username,
+                password=password
+            )
+            if authentica:
+                login(self.request, user=usuario)
+        
+        self.request.session['carrinho'] = self.carrinho
+        self.request.session.save()       
+        
+        return self.renderizar
 
 class Atualizar(View):
     pass
