5f32b11fec36348075f9a4c5a9d2b6aaaf120f35
rafael <august.rafael@gmail.com>
2024-06-07 20:43:31 -0400

    list_filter, search_fields para produtos e classe VariacaoAdmin

--- produto/admin.py
+++ produto/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
 from .models import Produto, Variacao
 
+class VariacaoAdmin(admin.ModelAdmin):
+    display_fields = ['nome','produto','preco','preco_promocional','estoque']
+    list_filter = ('produto','estoque',)
+    search_fields = ('nome',)
+    
 class ItemVariacaoInline(admin.TabularInline):
     model = Variacao
-    extra = 1
+    extra = 1   
+    
 
 class ProdutoAdmin(admin.ModelAdmin):
-    display_fields = ['nome', 'descricao_curta', 'preco_marketing', 'preco_marketing_promocional']
+    display_fields = ['nome', 'descricao_curta', 'preco_marketing', 'preco_marketing_promocional', 'slug']
     inlines = [
         ItemVariacaoInline
-    ]    
+    ]   
+    list_filter = ('slug',) 
+    search_fields = ('nome',)
 
 # Register your models here.
 admin.site.register(Produto, ProdutoAdmin)
-admin.site.register(Variacao)
+admin.site.register(Variacao, VariacaoAdmin)
 
