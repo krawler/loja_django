cd73809786d1d0304fa99d20add4397d5d8c15b1
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-07-05 08:04:11 -0300

    novas rotas

--- pedido/urls.py
+++ pedido/urls.py
@@ -5,6 +5,8 @@ app_name = "pedido"
 
 urlpatterns = [
    path('', views.Pagar.as_view(), name="pagar"),
-   path('salvarpedido/', views.SalvarPedido.as_view(), name="salvarpedido"),
-   path('detahe', views.Detalhe.as_view(), name="detalhe"),
+   path('salvarpedido', views.SalvarPedido.as_view(), name="salvarpedido"),
+   path('compraconcluida/', views.CompraConcluida.as_view(), name="compraconcluida"),   
+   path('meuspedidos', views.MeusPedidos.as_view(), name="meuspedidos"),
+   path('detahe/<int:pk>', views.Detalhe.as_view(), name="detalhe"),
 ]  
