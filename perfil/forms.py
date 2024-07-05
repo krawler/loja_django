8c8478297bee30b6536cdbc1d8c77016f4e0b490
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-07-05 09:19:32 -0300

    ajustes no forms do perfil

--- perfil/forms.py
+++ perfil/forms.py
@@ -1,12 +1,18 @@
 from django import forms
+from django.db.models import DateField
 from django.contrib.auth.models import User
 from . import models
 
 class PerfilForm(forms.ModelForm):
+    
+    def __init__(self, perfil=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.perfil = perfil
+         
     class Meta:
         model = models.PerfilUsuario
-        fields = '__all__'
-        exclude = ('usuario',)
+        fields = ('idade', 'data_nascimento', 'cpf', 'endereco', 'numero', 'complemento', 
+                  'bairro', 'cep', 'cidade', 'estado')
 
 class UserForm(forms.ModelForm):
     
@@ -53,13 +59,6 @@ class UserForm(forms.ModelForm):
         error_msg_password_short = 'A senha precisa de pelo menos 6 caracteres'
         
         if self.usuario:
-            if usuario_db:
-                if usuario_data == usuario_db.username:
-                    validation_error_msgs['username'] = error_msg_user_exists
-            
-            if email_db:
-                if email_data == email_db.email:                
-                    validation_error_msgs['email'] = error_msg_email_exists
             
             if password_data:
                 if password_data != password2_data:
@@ -67,9 +66,19 @@ class UserForm(forms.ModelForm):
                     validation_error_msgs['password2'] = error_msg_password_match
                 if len(password_data) < 6:
                     validation_error_msgs['password'] = error_msg_password_match
-        # Usuarios não logados: cadastro            
         else:
-            pass
+            if email_db:
+                if email_data == email_db.email:                
+                    validation_error_msgs['email'] = error_msg_email_exists
+            
+            if usuario_db:
+                validation_error_msgs['username'] = error_msg_user_exists
+           
+            if not password_data:
+                validation_error_msgs['password'] = error_msg_password_empty            
+            
+            if not password2_data:
+                validation_error_msgs['password2'] = error_msg_password_empty            
         
         if validation_error_msgs:
             raise(forms.ValidationError(validation_error_msgs))
