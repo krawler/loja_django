f910b843098ced5b77d7188cd9e032edfa40ad34
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-11-20 12:20:51 -0300

    novas configurações para funcionar no fastcomet

--- loja_django/settings.py
+++ loja_django/settings.py
@@ -51,6 +51,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,19 +128,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-# para remoto ativa STATIC_ROOT
-STATIC_ROOT = '/home5/raradmco/loja_django/static'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'live-static', 'media-root')
-
-STATICFILES_DIRS = [os.path.join('static')]
-
-# TODO: colocar o media url e root
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MESSAGE_TAGS = {
     constants.DEBUG: 'alert-info',
