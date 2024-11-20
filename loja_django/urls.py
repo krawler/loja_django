08f7bb141cf41d37a89c93433a3e3cd9e73a0669
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-11-20 12:21:37 -0300

    rota para os arquivos estaticos

--- loja_django/urls.py
+++ loja_django/urls.py
@@ -18,17 +18,11 @@ from django.contrib import admin
 from django.urls import path, include   
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('',include('produto.urls')),
     path('perfil/',include('perfil.urls')),
     path('pedido/',include('pedido.urls')),
-    path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# TODO: Remover debug toolbar
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    path('admin/', admin.site.urls)
+] + staticfiles_urlpatterns()
