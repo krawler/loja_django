4252c9656e9f48f6013dd6dfa705a127985fca8d
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-11-22 01:23:43 -0300

    implemeta o metodo get do Atualizar herdando Criar

--- perfil/views.py
+++ perfil/views.py
@@ -136,8 +136,13 @@ class Cadastro_concluido(View):
          }    
          return render(self.request, self.template_name, contexto)
 
-class Atualizar(View):
-    pass
+class Atualizar(Criar):
+    
+    def get(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return redirect('perfil:login')
+        
+        return self.renderizar    
 
 class Login(View):
 
