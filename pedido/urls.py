e85784cd6a1118c7b5aa0cfb1372fcb7ce74eafd
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-11-25 18:21:31 -0300

    novos metodos atualiza e desativar

--- pedido/urls.py
+++ pedido/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
    path('detalhe/<int:pk>', views.Detalhe.as_view(), name="detalhe"),
    path('tabela', views.Tabela.as_view(), name="tabela"),
    path('itens-pedido', views.ItensPedido_json.as_view(), name="itens_pedido"),
-   path('atualizapedido/', views.Atualizar_Pedido.as_view(), name="atualizapedido")
+   path('atualizapedido/', views.Atualizar_Pedido.as_view(), name="atualizapedido"),
+   path('desativar/', views.Desativar_Pedido.as_view(), name="desativar"),
 ]  
