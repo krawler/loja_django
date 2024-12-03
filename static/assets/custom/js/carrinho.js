b005ff369093c4a9eecd14c3d8933d1fe1cbc2dd
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-12-03 18:33:07 -0300

    alcançada a funcionalidade de alterar a quantidade de todos os itens do carrinho

--- static/assets/custom/js/carrinho.js
+++ static/assets/custom/js/carrinho.js
@@ -3,6 +3,7 @@ var urlCarrinhoPost = '';
 var urlDestinoContinuar = '';
 var variacao_id = null;
 var quantidade = 0;
+var quantidadeAlterada = false;
 $(document).ready(function(){
 
     $.ajaxSetup({
@@ -12,24 +13,49 @@ $(document).ready(function(){
 
     $("#continuar_comprando").on('click', function(e){
 
+        dialogChangeQuantity();
+    });
+
+    function isQuantidadeAlterada(){
         $('tr[data-quant]').each(function() {
             var $row = $(this);
-            quantidade = $row.find('.quantidade').val();
             var quantidadeOriginal = $row.data('quant');
-            variacao_id = $row.data('variacaoid');
-            urlCarrinhoPost = $(this).data('url');
-            urlDestinoContinuar = $(this).attr('url-destino');
+            quantidade  = $row.find('.quantidade').val();            
 
             if (quantidade != quantidadeOriginal) {
-                $("#dialogModal").show();
-            }else{
-                $("#dialogModal2").show();
-            }   
-        });                    
+                quantidadeAlterada = true;
+                return false;
+            }
+        });
+        if(quantidadeAlterada == true)
+            return true;
+        else
+            return false;
+    }
+
+    $("#closeDialogModal2").click(function(){
+        $("#dialogModal2").hide(); 
+    });
+
+    $("#closeDialogModal").click(function(){
+        $("#dialogModal").hide();
+    });
+
+    $("#goToStore").click(function(){
+        $("#dialogModal2").hide();
+    });
+
+    $("#finaliza_compra").click(function(){
+        dialogChangeQuantity();
+    });
+
+    $("#btnFecharDialog2").on('click', function(){
+        $("#dialogModal2").hide();     
     });
 
     $("#changeCartQuantity").click(function(){
         
+        $("#dialogModal").hide(); 
         $.ajax({
             type: "POST",
             dataType: "json",
@@ -39,21 +65,45 @@ $(document).ready(function(){
                 "quantidade": quantidade,
                 "csrfmiddlewaretoken": $('meta[name="csrf-token"]').attr('content')
             },
-            success: function(jsonData) { 
-                $("#dialogModal").hide();                
+            success: function(jsonData) {                
+                quantidadeAlterada = false;
+                $('tr[data-quant]').each(function() {
+                    var $row = $(this);
+                    if(variacao_id  == $row.data('variacaoid'))
+                        $row.data('quant', quantidade);
+                });
             }
         });
-                
-    });
-
-    $("#closeDialogModal2").click(function(){
-        $("#dialogModal2").hide(); 
-    });
-
-    $("#closeDialogModal").click(function(){
-        $("#dialogModal").hide();
+        dialogChangeQuantity();
     });
     
+    function dialogChangeQuantity(){
+        
+        if(isQuantidadeAlterada()){
+            $('tr[data-quant]').each(function() {
+                var $row = $(this);
+                var quantidadeOriginal = $row.data('quant');
+                var nomeProduto = $row.children('td.nome_produto').children('a').html();
+                quantidade             = $row.find('.quantidade').val();            
+                variacao_id            = $row.data('variacaoid');
+                urlCarrinhoPost        = $(this).data('url');
+                urlDestinoContinuar    = $(this).attr('url-destino');
+                
+    
+                if (quantidade != quantidadeOriginal) {
+                    quantidadeAlterada = true;
+                    $("#dialogModal").children('div')
+                                        .children('div')
+                                        .children('div.modal-body')
+                                        .html('Tem certeza que deseja alterar a quantidade? do produto ' + nomeProduto + ' de ' + quantidadeOriginal + ' para ' + quantidade)
+                    $("#dialogModal").show();
+                    return false;
+                }
+            });      
+        }
+        if(!isQuantidadeAlterada())
+            $("#dialogModal2").show();                     
+    }
 
 });
 
