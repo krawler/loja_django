36f66b1192f4983e25de6e86eb38214bb66a1863
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-12-02 23:09:22 -0300

    alteração do metodo trocando metodos do orm do django por sql puro

--- produto/produto_service.py
+++ produto/produto_service.py
@@ -1,12 +1,13 @@
 from django.views import View
 from pedido.models import ItemPedido
 from django.contrib.auth.models import User
+from django.db import connection
 from django.db.models import Q, Count, QuerySet, Sum
 from django.core import serializers
 from decimal import Decimal
 from datetime import datetime
 from django.utils import timezone
-from produto.models import Produto, SessaoCarrinho, Variacao, SaidaProduto, EntradaProduto, AcessoProduto
+from produto.models import Produto, SessaoCarrinho, Variacao, SaidaProduto, EntradaProduto, AcessoProduto, ProdutoMaisAcessado
 
 import json
 
@@ -99,3 +100,29 @@ class ProdutoService():
         produto = Produto.objects.filter(slug=slug).first()
         acesso = AcessoProduto(produto=produto, user=user)
         acesso.save()
+
+    def get_produtos_mais_acessados_por_usuario(request, user):
+        user = User.objects.filter(username=user).first()
+        if user == None:
+            return
+        with connection.cursor() as cursor:
+            str_sql = """
+                        SELECT p.nome, p.descricao, p.imagem, p.slug, p.preco_marketing, 
+                        p.preco_marketing_promocional, COUNT(ap.id) AS total_acessos
+                        FROM produto_acessoproduto ap
+                        INNER JOIN produto_produto p ON ap.produto_id = p.id
+                        WHERE ap.user_id = %s
+                        GROUP BY p.id
+                        ORDER BY total_acessos DESC
+                        LIMIT 4;
+                        """
+            cursor.execute(str_sql, [user.id])
+
+            rows = cursor.fetchall()
+            produtos = []
+            for row in rows:
+                produtos.append(ProdutoMaisAcessado(nome=row[0], descricao=row[1], imagem=row[2], 
+                                                    slug=row[3], preco=row[4], preco_promocional=row[5], 
+                                                    total_acessos=row[6]))
+
+        return produtos
