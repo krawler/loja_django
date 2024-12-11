5c7b2f0699564a88dc67d13f638e700b56b50a2a
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-12-11 00:47:09 -0300

    novo campo observações

--- pedido/models.py
+++ pedido/models.py
@@ -25,6 +25,7 @@ class Pedido(models.Model):
     )
     desativado = models.BooleanField(default=False)
     codigo_rastreio_correio = models.CharField(max_length=50, blank=True, null=True)
+    observacoes = models.TextField(max_length=1000, null=True)
 
     def __str__(self):
         return f'Pedido N. {self.pk}'
