4dbd727ce9df52ace0ce74a08b13f6c1f30d6395
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-12-16 21:15:58 -0300

    implementação do fancybox

--- static/assets/custom/js/detalhe_produto.js
+++ static/assets/custom/js/detalhe_produto.js
@@ -1,6 +1,43 @@
 $(document).ready(function(){
 
-    Fancybox.bind("[data-fancybox]", {});      
+    
+Fancybox.bind('[data-fancybox="gallery"]', {
+            compact: false,
+            idle: false,
+        
+            animated: false,
+            showClass: false,
+            hideClass: false,
+        
+            dragToClose: false,
+        
+            Images: {
+            // Disable animation from/to thumbnail on start/close
+            zoom: false,
+            },
+        
+            Toolbar: {
+            display: {
+                left: [],
+                middle: [],
+                right: ['close'],
+            },
+            },
+        
+            Thumbs: {
+            type: 'classic',
+            Carousel: {
+                center: function () {
+                return this.contentDim > this.viewportDim;
+                },
+            },
+            },
+        
+            Carousel: {
+            // Remove the navigation arrows
+            Navigation: false,
+            },
+        });     
 
     $("#imagem_produto").ezPlus();    
 
