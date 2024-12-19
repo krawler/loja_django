f3da3b94fb000a7ce876a3a36d37e258bec883de
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-12-19 13:10:05 -0300

    novo metodo checkout_pagseguro

--- pedido/pedido_service.py
+++ pedido/pedido_service.py
@@ -1,6 +1,7 @@
-from produto.models import ProdutoSimples
+from produto.models import ProdutoSimples, Variacao
 from .models import Pedido
 from django.db import connection
+from django.shortcuts import reverse
 from pagseguro import PagSeguro
 
 class Pedido_Service():
@@ -69,35 +70,57 @@ class Pedido_Service():
         return pedido
 
 
-    def checkout_pagseguro(self, user, variacoes):
-        pg = PagSeguro(email="seuemail@dominio.com", token="ABCDEFGHIJKLMNO")
-        #dados do usuario e do perfil
+    def checkout_pagseguro(self, request, carrinho, id_pedido):
+
+        user = request.user
         perfil = user.perfilusuario
-        pg.sender = {
-            "name": perfil.nome_completo,
-            "area_code": 14,
-            "phone": 996064031,
-            "email": "august.rafael@gmail.com",
-        }
-        #dados do perfil
-        pg.shipping = {
-            "type": pg.SEDEX,
-            "street": perfil.endereco,
-            "number": int(perfil.numero),
-            "complement": perfil.complemento,
-            "district": perfil.bairro,
-            "postal_code": perfil.cep,
-            "city": perfil.cidade,
-            "state": perfil.estado,
-            "country": "BRA"
-        }
-        for variacao in variacoes:
-            pg.items.append(
-                {"id": "0003", 
-                    "description": variacao.produto.descricao, 
-                    "amount": variacao.preco if variacao.preco_promocional is None else variacao.preco_promocional, 
-                    "quantity": 2, 
-                    "weight": 200},
+        config = {'sandbox': True}
+        pg = PagSeguro(email="august.rafael@gmail.com", token="9d724eb2-b076-4ec6-a3f6-2eb62e3be240f701023145f0bcf5fcf389ad5ee0602f587c-7946-4635-be65-30827c01c169", config=config)        
+        try:
+            pg.reference = id_pedido
+            '''
+            pg.sender = {
+                "name": "Iza Marina Viccino",
+                "area_code": "14",
+                "phone": "996064031",
+                "email": "iviccino@gmail.com",
+            }
+            pg.shipping = {
+                "type": pg.SEDEX,
+                "street": perfil.endereco,
+                "number": perfil.numero,
+                "complement": perfil.complemento,
+                "district": perfil.bairro,
+                "postal_code": perfil.cep,
+                "city": perfil.cidade,
+                "state": perfil.estado,
+                "country": "BRA"
+            }
+            '''
+            for item in carrinho:
+                variacao = Variacao.objects.get(id=item) 
+                pg.items.append(
+                    {"id": str(variacao.id), 
+                        "description": variacao.produto.nome + ' - ' + variacao.nome, 
+                        "amount": variacao.preco if variacao.preco_promocional is None else variacao.preco_promocional, 
+                        "quantity": 2, 
+                        "weight": float(variacao.peso),
+                        "shipping_cost": 10.00
+                })
+            pg.extra_amount = 5.70    
+            pg.redirect_url = request.build_absolute_uri(
+                                        reverse('pedido:compraconcluida')
             )
-        pg.redirect_url = "http://localhost:8000/pedido/salvarpedido"
-        response = pg.checkout()
+            pg.notification_url = request.build_absolute_uri(
+                                        reverse('pedido:compraconcluida')  #notification
+            )
+            response = pg.checkout()
+            return response.payment_url
+        except TypeError as e:
+            print("Erro de tipo:", e)
+            print("Verifique se todos os valores estão sendo passados corretamente.")
+        except AttributeError as e:
+            print("Erro de atributo:", e)
+            print("Verifique se os atributos estão sendo acessados corretamente.")    
+        except Exception as e:
+            print(e)
