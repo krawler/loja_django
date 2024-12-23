abc6e5bcdd7775345acd50038f110ee50850970c
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-12-22 23:18:11 -0300

    inclui configurações para evitar ssl

--- loja_django/settings.py
+++ loja_django/settings.py
@@ -162,3 +162,13 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
+
