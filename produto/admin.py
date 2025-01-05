c946da9ada95589fe6fada7282ac57d3a99616b2
Rafael Augusto Ramos <august.rafael@gmail.com>
2025-01-05 00:01:28 -0300

    Variação admin ajustado

--- produto/admin.py
+++ produto/admin.py
@@ -1,27 +1,22 @@
 from django.contrib import admin
 from .models import Produto, Variacao, ImagemProduto
 
+
 class imagemItemInline(admin.TabularInline):
     model = ImagemProduto
     extra = 1  
 
+
 class VariacaoAdmin(admin.ModelAdmin):
     display_fields = ['nome','produto','preco','preco_promocional','estoque']
-<<<<<<< HEAD
-    search_fields = ('nome',)
+    search_fields = ('nome', 'produto.nome')
     list_per_page = 15
-    list_filter = ('produto','estoque',)
-    
-=======
-    inlines = [
-        imagemItemInline
-    ] 
 
->>>>>>> main
+    
 class ItemVariacaoInline(admin.TabularInline):
     model = Variacao
-    extra = 1   
-    
+    extra = 1
+       
 
 class ProdutoAdmin(admin.ModelAdmin):
     display_fields = ['nome', 'descricao_curta', 'preco_marketing', 'preco_marketing_promocional', 'slug']
